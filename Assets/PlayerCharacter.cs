e4cb562d9c03c676f172e15acab745c98fc03305
Pinkello <heronavan@gmail.com>
2020-07-29 20:58:52 +0200

    Add shooting AI

---
+++ Assets/PlayerCharacter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PlayerCharacter: MonoBehaviour
+{
+    private int _health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _health = 5;
+    }
+
+    public void Hurt(int damage)
+    {
+        _health -= damage;
+        Debug.Log("Kondycja: " + _health);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
